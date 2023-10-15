a905916ebb6ea36babb5cdd5a7b77aa5b5f98ca6
Mizanur Rahaman Raihan <raihantfp@gmail.com>
2023-10-15 23:09:46 +0600

    Model fillable added

--- app/Models/UserSearchHistory.php
+++ app/Models/UserSearchHistory.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class UserSearchHistory extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'search_keyword',
+        'searched_at',
+    ];
+
 }
